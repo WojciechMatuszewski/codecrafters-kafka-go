804dda67142818cfd727d081a5946481aa75e843
Wojciech Matuszewski <wm.matuszewski@gmail.com>
2024-09-15 05:12:54 +0200

    update

--- app/server.go
+++ app/server.go
@@ -10,20 +10,23 @@ import (
 var _ = net.Listen
 var _ = os.Exit
 
-func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
+const (
+	Host string = "0.0.0.0"
+	Port string = "9092"
+)
 
-	// Uncomment this block to pass the first stage
-	//
-	// l, err := net.Listen("tcp", "0.0.0.0:9092")
-	// if err != nil {
-	// 	fmt.Println("Failed to bind to port 9092")
-	// 	os.Exit(1)
-	// }
-	// _, err = l.Accept()
-	// if err != nil {
-	// 	fmt.Println("Error accepting connection: ", err.Error())
-	// 	os.Exit(1)
-	// }
+func main() {
+	address := fmt.Sprintf("%s:%s", Host, Port)
+	l, err := net.Listen("tcp", address)
+	if err != nil {
+		fmt.Println("Failed to bind to port 9092", err)
+
+		os.Exit(1)
+	}
+
+	_, err = l.Accept()
+	if err != nil {
+		fmt.Println("Error accepting connection: ", err.Error())
+		os.Exit(1)
+	}
 }
