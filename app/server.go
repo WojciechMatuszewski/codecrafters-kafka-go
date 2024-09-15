178e73155d9b43426f616954360511672224d010
Wojciech Matuszewski <wm.matuszewski@gmail.com>
2024-09-15 06:35:39 +0200

    update

--- app/server.go
+++ app/server.go
@@ -1,32 +1,111 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net"
-	"os"
-)
-
-// Ensures gofmt doesn't remove the "net" and "os" imports in stage 1 (feel free to remove this!)
-var _ = net.Listen
-var _ = os.Exit
-
-const (
-	Host string = "0.0.0.0"
-	Port string = "9092"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
-	address := fmt.Sprintf("%s:%s", Host, Port)
-	l, err := net.Listen("tcp", address)
-	if err != nil {
-		fmt.Println("Failed to bind to port 9092", err)
+	const (
+		host string = "localhost"
+		port string = "9092"
+	)
 
-		os.Exit(1)
+	server := newServer(host, port)
+	err := server.Start()
+	if err != nil {
+		panic(err)
 	}
+}
 
-	_, err = l.Accept()
+type Server struct {
+	Host string
+	Port string
+
+	listener *net.Listener
+}
+
+func newServer(host string, port string) Server {
+	return Server{
+		Host: host,
+		Port: port,
+	}
+}
+
+func (s Server) Start() error {
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
+	address := fmt.Sprintf("%s:%s", s.Host, s.Port)
+
+	lc := net.ListenConfig{}
+	listener, err := lc.Listen(ctx, "tcp", address)
 	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+		return fmt.Errorf("failed to bind to port %s: %w", s.Port, err)
+	}
+	defer listener.Close()
+
+	go acceptLoop(ctx, listener)
+
+	<-ctx.Done()
+
+	fmt.Println("Shutting down")
+
+	return nil
+}
+
+func acceptLoop(ctx context.Context, listener net.Listener) {
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Println("Stopping to accept connections")
+			return
+		default:
+			connection, err := listener.Accept()
+			if err != nil {
+				if ctx.Err() != nil {
+					fmt.Println("Listener closed, stopping the accept loop")
+					return
+				}
+
+				fmt.Println("Error accepting the connection:", err)
+				continue
+			}
+
+			fmt.Println("New connection to the server:", connection.RemoteAddr())
+
+			go connectionLoop(connection)
+		}
+	}
+}
+
+func connectionLoop(connection net.Conn) {
+	defer connection.Close()
+
+	received := make([]byte, 1024)
+	for {
+		_, err := connection.Read(received)
+		if err != nil {
+			fmt.Println("Failed to read:", err)
+			continue
+		}
+
+		fmt.Println(string(received))
+
+		header := make([]byte, 4)
+		message := []byte{0x00, 0x00, 0x00, 0x07}
+
+		var res []byte
+		res = append(res, header...)
+		res = append(res, message...)
+
+		_, err = connection.Write(res)
+		if err != nil {
+			fmt.Println("Failed to write:", err)
+			continue
+		}
 	}
 }
